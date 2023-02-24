8604bc684803f558d69348d685feabef9b028388
jmonto55 <jmonto55@gmail.com>
2023-02-24 16:48:49 -0500

    Add Routers and Categories button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import BookStoreLogic from './components/BookStoreLogic';
+import Categories from './components/Categories';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div data-testid="app" className="app">
       <Navbar />
-      <BookStoreLogic />
-      <Routes />
+      <Routes>
+        <Route path="/" element={<BookStoreLogic />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
